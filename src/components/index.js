34e29ac2b6fa055b32d01552a3c9da5313170245
Arin Haritash <sharmavasu8765@gmail.com>
2024-07-24 11:41:22 +0530

    uploaded project

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Billing from "./Billing";
+import Business from "./Business";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Footer from "./Footer";
+
+export{
+    Navbar,
+    CTA,
+    CardDeal,
+    Billing,
+    Business,
+    Footer,
+    Stats,
+    Clients,
+    Hero,
+    Testimonials
+
+}
